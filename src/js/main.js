39a91268c136c0480a505bd5d55182dc6206b712
nltesown <nlte@magic.fr>
2016-09-08 11:56:11 +0200

    Debug viewer script

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,26 @@
 $(window).on("load", function () {
-
-  var $activeShape = $("");
-
-
-  var w = $("svg").eq(0).width();
-
-
+  var queue = []; // A queue of the shape(s) _to be opened_
+  var $shape = null;
+  // var w = $("svg").eq(0).width();
 
   viewer.init();
 
+  $(".shape").on("click", function () {
+    queue.push($(arguments[0].target));
+    viewer.close(); // The only action here is to close the viewer (be it opened or not) -- the event handler will take care of opening the shape in queue
+  });
 
-  $(".shape").on("click", viewerTrigger);
+  viewer.on("viewer.close", function () {
+    ($shape || $("")).removeClass("on");
 
+    if (queue.length > 0) {
+      $shape = queue.pop();
+      $shape.addClass("on");
 
-
-
-  function viewerTrigger () {
-    $activeShape.removeClass("on");
-    $activeShape = $(arguments[0].target);
-    $activeShape.addClass("on");
-
-
-    console.log($activeShape.offset().left);
-
-    function viewerOpen() {
-      viewer.content($activeShape.data("info"));
-      viewer.open();
+      viewer
+      .content($shape.data("info"))
+      .open();
     }
-
-    if (viewer.isOpen() === true) {
-      viewer.close();
-      viewer.on("viewer.close", viewerOpen);
-    } else {
-      viewerOpen();
-    }
-
-  }
+  });
 
 });
--- src/js/viewer.js
+++ src/js/viewer.js
@@ -15,18 +15,17 @@ var viewer = (function () {
 
     $viewer.append("<div class='viewerContent'></div><div class='viewerClose'></div>");
     $viewerContent = $viewer.children(".viewerContent");
-    // $viewerClose = $viewer.children(".viewerClose").hide();
     $viewerClose = $viewer.children(".viewerClose");
     $viewer.perfectScrollbar({
       suppressScrollX: true,
       wheelSpeed: 3
     });
-    console.log("Viewer initialized");
-    return $viewer; // Test
+    return this;
   }
 
   function on(event, callback) {
     $.subscribe(event, callback);
+    return this;
   }
 
   function open() {
@@ -37,8 +36,10 @@ var viewer = (function () {
       .one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () {
         isOpen = true;
         $.publish("viewer.open");
+        $viewerClose.one("click", close);
       });
     }
+    return this;
   }
 
   function close() {
@@ -50,13 +51,15 @@ var viewer = (function () {
         isOpen = false;
         $.publish("viewer.close");
       });
+    } else {
+      $.publish("viewer.close"); // The close event is published even when the viewer _was_ already closed
     }
+    return this;
   }
-
-
 
   function content(html) {
     $viewerContent.empty().html(html);
+    return this;
   }
 
   return {
@@ -68,107 +71,3 @@ var viewer = (function () {
     isOpen: function () { return isOpen; }
   };
 })();
-
-
-
-
-/*
-var viewer = (function () {
-
-  var isOpen = false;
-  var scaleRatio = 40;
-  var duration = 350;
-  var node = {};
-  var template;
-  var $viewer;
-  var $viewerClose;
-  var $viewerContent;
-
-  function init(_$viewer, templateString) {
-    $viewer = _$viewer;
-    template = _.template(templateString);
-    $viewer.css({ width: "0px", height: "0px" });
-    $viewer.append("<div class='viewerContent'></div><div class='viewerClose'></div>");
-    $viewerContent = $viewer.children(".viewerContent");
-    $viewerClose = $viewer.children(".viewerClose").hide();
-    $viewer.perfectScrollbar({
-      suppressScrollX: true,
-      wheelSpeed: 3
-    });          
-  }
-
-
-  function on(event, callback) {
-    $.subscribe(event, callback);
-  }
-
-
-  function open(_node) {
-    node = _node;
-    if (isOpen === true) return;
-    if (node.level !== 2) return;
-    $viewer.css({ top: node.y + "px", left: node.x + "px" });
-
-    window.setTimeout(function () { // Important: timeout allows top/left position to be properly set before transitioning
-      $viewer.css({
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-        "transition-duration": duration + "ms",
-        "transition-timing-function": "cubic-bezier(0.420, 0.000, 0.580, 1.000)"
-      })
-      .one("transitionend", function () {
-        $viewerContent.append(template(node));
-
-        $viewer.css({ "transition-duration": 0 }); // Important to prevent $viewer animation on window resize
-        $viewer.scrollTop(0).perfectScrollbar("update");
-
-        $viewerClose.show();
-        $viewerClose.one("click", close);
-        $(document).on("keydown", function(e) {
-          if (e.which === 27) {
-            $(document).off("keydown");
-            $viewerClose.addClass("on");
-          }
-        });
-        $(document).on("keyup", function (e) { // Close with Escape key
-          if (e.which  === 27) {
-            $(document).off("keyup");
-            $viewerClose.removeClass("on");
-            close();
-          }
-        });
-
-        isOpen = true;
-      });
-    }, 1);
-  }
-
-  function close() {
-    if (isOpen === false) return;
-    $viewerClose.hide();
-    $viewerContent.empty();
-    $viewer
-    .css({
-      top: node.y + "px",
-      left: node.x + "px",
-      width: 0,
-      height: 0,
-      "transition-duration": duration + "ms",
-      "transition-timing-function": "cubic-bezier(0.420, 0.000, 0.580, 1.000)"
-    })
-    .one("transitionend", function () {
-      isOpen = false;
-      $.publish("viewer.close");
-    });
-  }
-
-  return {
-    close: close,
-    init: init,
-    on: on,
-    open: open
-  };
-})();
-*/
